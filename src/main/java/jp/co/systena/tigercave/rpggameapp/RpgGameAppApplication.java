210f205556c45e2ea4c85c21630fc81254128020
ezurar <ezurar@systena.co.jp>
2019-08-28 19:06:11 +0900

    初期コミット

---
+++ src/main/java/jp/co/systena/tigercave/rpggameapp/RpgGameAppApplication.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigercave.rpggameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgGameAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgGameAppApplication.class, args);
+	}
+
+}
